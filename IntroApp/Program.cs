80e4d7db0037e8e0d6bb3b4c2d2a8a4c180f5587
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-11-15 11:02:57 +0000

    feat: create new console app; try coding exercises

--- IntroApp/Program.cs
+++ IntroApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Problem 1
+// public class Program
+// {
+// // Method to calculate the final price after a discount
+// public static double ApplyDiscount(double price, double discountPercentage)
+// {
+//     return price - (price * (discountPercentage / 100));
+// }
+
+// public static void Main()
+// {   
+//     // Correct asnwer is 850
+//     double finalPrice = ApplyDiscount(1000, 15);
+//     Console.WriteLine("The final price is: " + finalPrice);
+// }
+// }
+
+// Problem 2
+// public class Program
+// {
+//     public static int FindMax(int[] numbers)
+//     {
+//         int max = numbers[0];
+//         for (int i = 0; i < numbers.Length; i++)
+//         {
+//             if (numbers[i] > max)
+//             {
+//                 max = numbers[i];
+//             }
+//         }
+//         return max;
+//     }
+
+//     public static void Main()
+//     {
+//         // Correct answer is -2
+//         int[] myNumbers = { -5, -10, -3, -8, -2 };
+//         int maxNumber = FindMax(myNumbers);
+//         Console.WriteLine("The maximum number is: " + maxNumber);
+//     }
+// }
---
+++ Palculator/Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Palculator;
+
+class Program {
+    static void Main()
+    {
+        Cal = new TwoNumCalculator();
+    }
+}
+
+// Classes
+public class TwoNumCalculator()
+{
+    public static double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public static double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public static double Multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public static double? Divide(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: Cannot divide by zero.");
+            return null;
+        }
+        return num1 / num2;
+    }
+    
+    public static void DisplayNumbers()
+    {
+        Console.WriteLine("Counting from 1 to 10...");
+        for (int i = 1; i < 11; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
